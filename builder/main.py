879d7fccccbb433687c96b71fbc74e59bc091598
Georgi Angelov <the.wizarda@gmail.com>
2021-10-08 16:04:21 +0300

    Merge pull request #82 from ivankravets/patch-2
    Specify detailed requirements for "toolchain-gccarmnoneeabi"

---
+++ builder/main.py
@@ -0,0 +1,35 @@
+# WizIO 2021 Georgi Angelov
+#   http://www.wizio.eu/
+#   https://github.com/Wiz-IO/wizio-pico
+
+from __future__ import print_function
+from os.path import join
+from SCons.Script import (AlwaysBuild, Builder, COMMAND_LINE_TARGETS, Default, DefaultEnvironment)
+from colorama import Fore
+from pioasm import dev_pioasm
+
+env = DefaultEnvironment()
+print( '<<<<<<<<<<<< ' + env.BoardConfig().get("name").upper() + " 2021 Georgi Angelov >>>>>>>>>>>>" )
+
+dev_pioasm(env)
+
+elf = env.BuildProgram()
+src = env.ElfToBin( join("$BUILD_DIR", "${PROGNAME}"), elf )
+prg = env.Alias( "buildprog", src, [ env.VerboseAction("", "DONE") ] )
+AlwaysBuild( prg )
+
+upload = env.Alias("upload", prg, [ 
+    env.VerboseAction("$UPLOADCMD", "Uploading..."),
+    env.VerboseAction("", ""),
+])
+AlwaysBuild( upload )    
+
+debug_tool = env.GetProjectOption("debug_tool")
+if None == debug_tool:
+    Default( prg )
+else:   
+    if 'cmsis-dap' in debug_tool:
+        Default( upload )
+    else:
+        Default( prg )
+
